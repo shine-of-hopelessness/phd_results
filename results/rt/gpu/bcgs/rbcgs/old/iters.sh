dd4cb5d8fe9a292a397e10bb78c79f9e2aa16c52
Vladimir Platonov <vplatonov@psgcluster.cm.cluster>
2016-05-27 15:29:00 -0700

    bicgs

---
+++ results/rt/gpu/bcgs/rbcgs/old/iters.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#grep "ExecutionTime" 1_4.out | tail -n1 | awk '{print $3}'
+#res=0
+#grep "ExecutionTime" $1 | head -n1 | awk '{res -= $3}'
+#grep "ExecutionTime" $1 | tail -n1 | awk '{res += $3}'
+#echo $res
+for f in *.out; do
+	echo $f
+	grep "iters" $f | awk '{sum += $2} END {print sum}'
+	#grep "exec time" $f | tail -n1 | awk '{print $4; print $8}'
+	#grep "ExecutionTime" $f | sed -e 1b -e '$!d' | awk 'NR>1{print $3-p} {p=$3}'
+done
